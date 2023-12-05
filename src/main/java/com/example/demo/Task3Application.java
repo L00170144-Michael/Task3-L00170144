468d47313996f56d18a8fc3e35bc2192c3e0f08c
Michael <L00170144@atu.ie>
2023-12-05 12:15:35 +0000

    Added initial commit with rest controller

---
+++ src/main/java/com/example/demo/Task3Application.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Task3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task3Application.class, args);
+	}
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+			return "Welcome!";
+		}
+	
+
+}
